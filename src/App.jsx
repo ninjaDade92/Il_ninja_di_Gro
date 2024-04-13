c6255e531462d4ec044bef973dc0e78c46aff1c4
Davide Lucchetti <davide.lucchetti@polomanettiporciatti.edu.it>
2024-04-13 12:00:56 +0200

    change

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import './App.css'
 
 function FilterableProductTable() {
 
-  const [filterText, setFilterText] = useState('');
-  const [inStockOnly, setInStockOnly] = useState(false);
+  const [filterText, setFilterText] = useState(''); //Inizializzo Searchbar
+  const [inStockOnly, setInStockOnly] = useState(false);  //Inizializzo la checkbox
+/*prima è variabile di stato, seconda è una 
+funzione che permette  di usare la variabile di stato*/
   
   let prodotti = [
     { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
@@ -14,12 +16,16 @@ function FilterableProductTable() {
     { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
     { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
   ];
-
-  return (
+      //uso le variabili di stato. la componente SearchBar gli passiamo il testo e flaggare la checkbox a seconda dello stato
+      //aggiungo 2 parametri alla search bar (filterText, inStockOnly)
+   return (
     <div class="FilterableProductTable">
       <SearchBar 
         filterText={ filterText }
-        inStockOnly={ inStockOnly }/>
+        inStockOnly={ inStockOnly }
+        setInStockOnly={setInStockOnly}
+        setFilterText={setFilterText} /> 
+
       <ProductTable 
         prodotti={ prodotti } 
         filterText={ filterText }
@@ -28,12 +34,12 @@ function FilterableProductTable() {
   )
 }
 
-function SearchBar() {
+function SearchBar({ filterText, inStockOnly, setInStockOnly, setFilterText }) { //passo i 4 parametri (2 variabili di stato, 2 funzioni per modificarle)
   return (
     <div class="SearchBar">
-      <input type="text" id="search" placeholder="Search..." />
-      <label>
-        <input type="checkbox" id="instock"/>
+      <input type="text" id="search" placeholder="Search..." value= { filterText } onChange= { e  => setFilterText(e.target.value)}/> 
+      <label>  
+        <input type="checkbox" id="instock" checked= { inStockOnly } onChange={ e => setInStockOnly(e.target.checked) }/>
         Only show products in stock
       </label>
     </div>
@@ -62,12 +68,25 @@ function ProductCategoryRow({ categoria }) {
   )
 } 
 
-function ProductTable({ prodotti }) {
-  let righe = [];
+function ProductTable({ prodotti, filterText, inStockOnly }) { // 3 parametri
+  let righe = []; //vettore vuoto, ogni  elemento sarà una riga
+  
+  //prima di creare la tabella elimina tutti i prodotti che hanno inStockOnly = a false
+  if (inStockOnly){
+    prodotti = prodotti.filter (p => p.stocked === true);
+  }
 
-  righe.push(<ProductCategoryRow categoria="Fruits" />);
-  let fruits = prodotti.filter(p => p.category === "Fruits");
+  if (filterText !== "") {
+    prodotti = prodotti.filter (p => p.name.toLowerCase().includes(filterText.toLowerCase())) /*prendo il 
+    vettore prodotti e mantieni solo i prodotti p il cui nome convertito in minuscolo contiene il testo minuscolo*/
+    //se ho scritto apple mi farà vedere solo Apple nella ProductTable
+  }
+  righe.push(<ProductCategoryRow categoria="Fruits" />); //inserisce intestazione di categoria con  push
+  let fruits = prodotti.filter(p => p.category === "Fruits"); /*crea vettore fruits e lo filtra
+                                                  (trattengo tutti i prodotti e li trattengo per categoria fruits)*/
   fruits.forEach(p => righe.push(<ProductRow nome={p.name} prezzo={p.price} stock={p.stocked} />));
+  //per ogni elemento del vettore fruits prendi l'elemento e inseriscilo in una riga con nome, prezzo, stocked
+  
 
   righe.push(<ProductCategoryRow categoria="Vegetables" />);
   let vegetables = prodotti.filter(p => p.category === "Vegetables");
